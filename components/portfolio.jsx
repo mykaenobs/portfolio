1ac0265dfa2f2b93dbbb0e091cdd5c4a3f9ab233
Myka Enobs <mykaenobs@gmail.com>
2021-11-09 17:35:27 +0100

    working

--- components/portfolio.jsx
+++ components/portfolio.jsx
@@ -5,12 +5,12 @@ const Portfolio = ({ reversed }) => {
   return (
     <section className={"max-w-wrapper ml-auto mr-auto flex justify-between " + reversed + " items-center gap-32 mt-20"}>
       <div className="bg-grey-100 flex-1 h-80 relative">
-        <div className="transform rotate-6 w-full h-full">
+        <div className="transform rotate-6 w-full h-full bg-red-200">
           <Image src="/images/project-01.png" className="transform" alt="Project one" layout="fill" />
         </div>
       </div>
       <div className="flex-1">
-        <small className="flex items-center gap-2 text-red-200">
+        <small className="flex items-center gap-2 text-red-100">
           <hr className="border-red-100 w-4" />
           <span>Project 1</span>
         </small>
